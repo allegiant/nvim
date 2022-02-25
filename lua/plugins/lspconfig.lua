762a2990a2ecd89f2ee6c02ceccab55fcfdd78f0
youngjion.xu@gmail.com <uwkri.fENPm6m3a>
2022-02-25 14:53:40 +0800

    feat(lspconfig): add goto_definition to split

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,38 @@ local lspconfig_util = require "lspconfig.util"
 local vim_api = vim.api
 local vim_lsp = vim.lsp
 
+local function goto_definition(split_cmd)
+  local util = vim.lsp.util
+  local log = require "vim.lsp.log"
+  local api = vim.api
+
+  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
+  local handler = function(_, result, ctx)
+    if result == nil or vim.tbl_isempty(result) then
+      local _ = log.info() and log.info(ctx.method, "No location found")
+      return nil
+    end
+
+    if split_cmd then
+      vim.cmd(split_cmd)
+    end
+
+    if vim.tbl_islist(result) then
+      util.jump_to_location(result[1])
+
+      if #result > 1 then
+        util.set_qflist(util.locations_to_items(result))
+        api.nvim_command "copen"
+        api.nvim_command "wincmd p"
+      end
+    else
+      util.jump_to_location(result)
+    end
+  end
+
+  return handler
+end
+
 local function on_attach(client, bufnr)
   client.resolved_capabilities.document_formatting = false
   client.resolved_capabilities.document_range_formatting = false
@@ -126,6 +158,7 @@ lspconfig_configs.volar_html = {
 local M = {}
 
 M.setup = function()
+vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
   -- lspconfig.volar_api.setup {
   --   on_attach = on_attach,
   --   flags = {
