de286e36987ec7e847c83f64d40df5248efd42fd
alegt <allegiant.xu@hotmail.com>
2022-05-05 22:21:23 +0800

    refactor(lspconfig~lspinstallerls): remove config's error

--- lua/plugins/lspinstaller.lua
+++ lua/plugins/lspinstaller.lua
@@ -2,6 +2,7 @@ local lsp_installer = require "nvim-lsp-installer"
 local vim_api = vim.api
 local vim_lsp = vim.lsp
 
+
 local capabilities = vim_lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
@@ -9,8 +10,8 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- do something on lsp on_attach
 local function on_attach(client, bufnr)
-  --client.resolved_capabilities.document_formatting = false
-  --client.resolved_capabilities.document_range_formatting = false
+  client.resolved_capabilities.document_formatting = false
+  client.resolved_capabilities.document_range_formatting = false
 
   -- set mappings only in current buffer with lsp enabled
   local function buf_set_keymap(...)
@@ -26,43 +27,24 @@ local function on_attach(client, bufnr)
   require("core.mappings").lsp(buf_set_keymap)
 end
 
--- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
--- or if the server is already installed).
-lsp_installer.on_server_ready(function(server)
+
+local installed_servers = lsp_installer.get_installed_servers()
+
+for _, server in pairs(installed_servers) do
   local opts = {
     on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    },
     capabilities = capabilities,
   }
-  -- (optional) Customize the options passed to the server
-  -- if server.name == "tsserver" then
-  --     opts.root_dir = function() ... end
-  -- end
-
-  -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-  -- before passing it onwards to lspconfig.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-
-  -- if server.name == "volar" then
-  --   opts.filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
-  -- end
-  -- server:setup(opts)
   if server.name == "rust_analyzer" then
-    -- Initialize the LSP via rust-tools instead
     require("rust-tools").setup {
       server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
     }
     server:attach_buffers()
-    -- Only if standalone support is needed
     require("rust-tools").start_standalone_if_required()
-  -- elseif server.name ~= "volar" then
   elseif server.name == "volar" then
-    opts.filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
+    opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" }
     server:setup(opts)
   else
     server:setup(opts)
   end
-end)
+end
