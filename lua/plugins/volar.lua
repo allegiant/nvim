6825cbb6b921c99a6a41a537b47165258838376a
alegt <allegiant.xu@hotmail.com>
2022-05-25 21:53:30 +0800

    feat(lsp): volar config change

--- lua/plugins/volar.lua
+++ lua/plugins/volar.lua
@@ -20,14 +20,132 @@ end
 local volar_cmd = { 'vue-language-server', '--stdio' }
 local volar_root_dir = lspconfig_util.root_pattern 'package.json'
 
+local filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' };
+local filetypes_with_json = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
+
+local settings = {
+  volar = {
+    codeLens = {
+      scriptSetupTools = true,
+      references = true,
+      pugTools = true,
+    },
+    lowPowerMode = false,
+    formatting = { printWidth = 100 },
+    autoCompleteRefs = true,
+    -- what am i smoking this is a vscode client option
+    takeOverMode = { enabled = true }, -- default is "auto" which launches only when builtin vscode TS ext is enabled. wonder how that logic behaves in neovim where there's no such builtin TS ext
+    completion = {
+      preferredTagNameCase = 'pascal',
+      preferredAttrNameCase = 'kebab',
+      autoImportComponent = true,
+    },
+    preview = {
+      port = 3333,
+      backgroundColor = '#fff',
+      transparentGrid = true,
+    }
+  },
+  ['volar-api'] = {
+    trace = {
+      server = 'verbose'
+    }
+  },
+  ['volar-document'] = {
+    trace = {
+      server = 'verbose'
+    }
+  },
+  ['volar-html'] = {
+    trace = {
+      server = 'verbose'
+    }
+  },
+}
+
+local commands = {
+  VolarHtmlToPug = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.html-to-pug',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarPugToHtml = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.pug-to-html',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarUseSetupSugar = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.use-setup-sugar',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarUnuseSetupSugar = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.unuse-setup-sugar',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarUseRefSugar = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.use-ref-sugar',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarUnuseRefSugar = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.unuse-ref-sugar',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarShowReferences = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.show-references',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarConvertToKebabCase = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.server.executeConvertToKebabCase',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+  VolarConvertToPascalCase = {
+    function()
+      vim.lsp.buf.execute_command({
+        command = 'volar.server.executeConvertToPascalCase',
+        arguments = { vim.uri_from_bufnr(0) }
+      })
+    end,
+  },
+}
+
 lspconfig_configs.volar_api = {
   default_config = {
     cmd = volar_cmd,
     root_dir = volar_root_dir,
     on_new_config = on_new_config,
-    -- filetypes = { 'vue' },
-    -- If you want to use Volar's Take Over Mode (if you know, you know)
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+    settings = settings,
+    commands = commands,
+    filetypes = filetypes_with_json,
     init_options = {
       typescript = {
         serverPath = ''
@@ -60,10 +178,8 @@ lspconfig_configs.volar_doc = {
     cmd = volar_cmd,
     root_dir = volar_root_dir,
     on_new_config = on_new_config,
-
-    filetypes = { 'vue' },
-    -- If you want to use Volar's Take Over Mode (if you know, you know):
-    --filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+    settings = settings,
+    filetypes = filetypes_with_json,
     init_options = {
       typescript = {
         serverPath = ''
@@ -74,9 +190,9 @@ lspconfig_configs.volar_doc = {
         documentLink = true,
         codeLens = { showReferencesNotification = true },
         -- not supported - https://github.com/neovim/neovim/pull/15723
-        semanticTokens = false,
-        diagnostics = true,
-        schemaRequestService = true,
+        -- semanticTokens = false,
+        diagnostics = { getDocumentVersionRequest = false }, -- if you set this to true it'll crash volar-doc with "MethodNotFound: vue/docVersion"
+          schemaRequestService = { getDocumentContentRequest = false } -- dunno if this crashes the ls but I'm disabling because I'm scared
       }
     },
   }
@@ -87,10 +203,8 @@ lspconfig_configs.volar_html = {
     cmd = volar_cmd,
     root_dir = volar_root_dir,
     on_new_config = on_new_config,
-
-    filetypes = { 'vue' },
-    -- If you want to use Volar's Take Over Mode (if you know, you know), intentionally no 'json':
-    --filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+    settings = settings,
+    filetypes = filetypes,
     init_options = {
       typescript = {
         serverPath = ''
@@ -102,9 +216,10 @@ lspconfig_configs.volar_html = {
         documentSymbol = true,
         -- not supported - https://github.com/neovim/neovim/pull/13654
         documentColor = false,
-        documentFormatting = {
-          defaultPrintWidth = 100,
-        },
+        -- documentFormatting = {
+        --   defaultPrintWidth = 100,
+        --   getDocumentPrintWidthRequest = 100
+        -- },
       }
     },
   }
