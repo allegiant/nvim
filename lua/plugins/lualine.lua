eaf95184fce305b22e908c35f145205e7fa32da7
xuyongjin <youngjion.xu@gmail.com>
2023-02-01 08:53:30 +0800

    refactor(lualine): add lazy.nvim status

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,6 +11,13 @@ local default = {
     theme = "gruvbox",
   },
   sections = {
+    lualine_x = {
+      {
+        require("lazy.status").updates,
+        cond = require("lazy.status").has_updates,
+        color = { fg = "#ff9e64" },
+      },
+    },
     lualine_y = {
       "lsp_progress",
     },
