fdfba1dc1c5b3b2f4ba35ebffd231f8ef3efbf5e
allegiant <allegiant.xu@hotmail.com>
2023-02-16 13:12:22 +0800

    refactor(autopairs): remove cmp confirm

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,36 +1,19 @@
 local present, autopairs = pcall(require, "nvim-autopairs")
+local Rule = require("nvim-autopairs.rule")
 if not present then
   return
 end
 
-local function cmp_setting()
-  autopairs.setup({
-      check_ts = true,
-      ts_config = {
-          rust = {},
-      },
-  })
-
-  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  local cmp = require('cmp')
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-
-  local Rule = require("nvim-autopairs.rule")
-  local ts_conds = require("nvim-autopairs.ts-conds")
-  autopairs.add_rules({
-      Rule("|", "|", "rust"),
-  })
-  autopairs.get_rule("'")[2]:with_pair(ts_conds.is_not_ts_node({ "type_arguments", "bounded_type" })) -- rust life
-end
-
 local M = {}
 
-local default = {
-
-}
-
 M.setup = function()
-  autopairs.setup(default)
-  cmp_setting()
+  autopairs.setup({
+    check_ts = true,
+  })
+
+  autopairs.add_rules({
+    Rule("|", "|", "rust"),
+    Rule("<", ">", "rust"),
+  })
 end
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,7 +20,6 @@ local default = {
     },
     indent = {
         enable = true,
-        disable = { "vue" }
     },
     highlight = {
         enable = true,
