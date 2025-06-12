ba7a61feef48a9f02804ebe207242eb001b1622c
xuyongjin <xuyongjin@zj-caiyu.com>
2025-06-12 08:39:47 +0800

    change rust

--- lua/lsp/vue_ls.lua
+++ lua/lsp/vue_ls.lua
@@ -1,32 +1,32 @@
-vim.lsp.config('volar', {
-  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-  init_options = {
-    vue = {
-      hybridMode = false,
-    },
-  },
-  settings = {
-    typescript = {
-      inlayHints = {
-        enumMemberValues = {
-          enabled = true,
-        },
-        functionLikeReturnTypes = {
-          enabled = true,
-        },
-        propertyDeclarationTypes = {
-          enabled = true,
-        },
-        parameterTypes = {
-          enabled = true,
-          suppressWhenArgumentMatchesName = true,
-        },
-        variableTypes = {
-          enabled = true,
-        },
-      },
-    },
-  },
+vim.lsp.config("vue_ls", {
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	init_options = {
+		vue = {
+			hybridMode = false,
+		},
+	},
+	settings = {
+		typescript = {
+			inlayHints = {
+				enumMemberValues = {
+					enabled = true,
+				},
+				functionLikeReturnTypes = {
+					enabled = true,
+				},
+				propertyDeclarationTypes = {
+					enabled = true,
+				},
+				parameterTypes = {
+					enabled = true,
+					suppressWhenArgumentMatchesName = true,
+				},
+				variableTypes = {
+					enabled = true,
+				},
+			},
+		},
+	},
 })
-vim.lsp.config['ts_ls'] = {}
-vim.lsp.enable('volar')
+vim.lsp.config["ts_ls"] = {}
+vim.lsp.enable("vue_ls")
--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,14 +1,25 @@
 return {
-  'mrcjkb/rustaceanvim',
-  version = '^5', -- Recommended
-  config = function()
-    vim.g.rustaceanvim = {
-      server = {
-        capabilities = require("blink.cmp").get_lsp_capabilities(),
-      },
-      dap = {
+	"mrcjkb/rustaceanvim",
+	version = "^5", -- Recommended
+	config = function()
+		vim.g.rustaceanvim = {
+			server = {
+				capabilities = require("blink.cmp").get_lsp_capabilities(),
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = false,
+						},
+						checkOnSave = false,
+						files = {
+							watcher = "client",
+						},
+					},
+				},
+			},
 
-      }
-    }
-  end
+			dap = {},
+		}
+	end,
 }
