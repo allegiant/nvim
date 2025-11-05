eeb5a3f89569e204c58c227f505a208b40e985f8
xuyongjin <xuyongjin@zj-caiyu.com>
2025-11-05 10:32:09 +0800

    lualine
    delete part of config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,24 +2,23 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    -- "arkav/lualine-lsp-progress",
   },
   opts = {
     options = {
       theme = "gruvbox-material",
     },
-    sections = {
-      lualine_x = {
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
-          color = { fg = "#ff9e64" },
-        },
-      },
-      lualine_y = {
-        "progress",
-        -- "lsp_progress",
-      },
-    }
+    -- sections = {
+    --   -- lualine_x = {
+    --   --   {
+    --   --     require("lazy.status").updates,
+    --   --     cond = require("lazy.status").has_updates,
+    --   --     color = { fg = "#ff9e64" },
+    --   --   },
+    --   -- },
+    --   lualine_y = {
+    --     "progress",
+    --     -- "lsp_progress",
+    --   },
+    -- }
   }
 }
